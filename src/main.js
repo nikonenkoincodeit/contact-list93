f5201a4fca95cdcf07c422d524316a14578614a8
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-10-29 10:40:48 +0200

    form-data

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+import { formEl } from './refs';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
+formEl.addEventListener('submit', onFormInput);
+
+function onFormInput(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  e.target.reset();
+}
