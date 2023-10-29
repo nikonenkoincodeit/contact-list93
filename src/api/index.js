6e6f6eae1f0ba5365cad1827b3473d3878d3e4da
bonny-art <svitlana.otenko@gmail.com>
2023-10-29 11:06:23 +0200

    adds serviseWriteData

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,36 @@
 const BASE_URL = "http://localhost:3000/contacts/";
+
+// function serviceWriteData(data) {
+//   const options = {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(data),
+//   };
+//   return fetch(BASE_URL, options).then((resp) => {
+//     console.log(resp);
+//     if (!resp.ok) {
+//       throw new Error(
+//         resp.statusText || `Unknown Error. Response status ${resp.status}`
+//       );
+//     }
+//     return resp.json();
+//   });
+// }
+
+async function serviceWriteData(data) {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+  const resp = await fetch(BASE_URL, options);
+
+  if (!resp.ok) {
+    throw new Error(
+      resp.statusText || `Unknown Error. Response status ${resp.status}`
+    );
+  }
+  return resp.json();
+}
+
+export { serviceWriteData };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { formEl } from './refs';
+import { formEl } from "./refs";
+import { serviceWriteData } from "./api";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
 
-formEl.addEventListener('submit', onFormInput);
+formEl.addEventListener("submit", onFormInput);
 
 function onFormInput(e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const data = Object.fromEntries(formData);
-  console.log(data);
   e.target.reset();
+  data.createdAt = Date.now();
+  serviceWriteData(data).then(console.log).catch(console.log);
 }
