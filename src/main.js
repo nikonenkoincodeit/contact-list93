43dd90895c22aa7a194fe6f55217baeaffd65225
AQR88 <timahaua@gmail.com>
2023-10-29 11:12:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,3 @@ import { serviceWriteData } from "./api";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
-
-formEl.addEventListener("submit", onFormInput);
-
-function onFormInput(e) {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const data = Object.fromEntries(formData);
-  e.target.reset();
-  data.createdAt = Date.now();
-  serviceWriteData(data).then(console.log).catch(console.log);
-}
