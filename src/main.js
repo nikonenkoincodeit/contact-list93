206735f983c4ea5c90d36d2a91c74f0e8fd0d5ba
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-11-11 15:39:18 +0200

    add submitForm

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", submitForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  const objUserData = Object.fromEntries(new FormData(evt.target));
+    objUserData.createdAt = Date.now();
+    evt.target.reset()
+}
